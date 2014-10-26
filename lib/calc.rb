c3f62db1a443a8e88193d5e59ae827c44747ca72
Fan Jiang <fanjiang@thoughtworks.com>
2014-10-26 22:39:42 +0800

    priority naive implemented

--- lib/calc.rb
+++ lib/calc.rb
@@ -16,32 +16,45 @@ module Calc
   end
 
   def self.eval(string)
-    nums, operations = tokenize(string)
-    result = evaluate(nums, operations)
+    token_stacks = tokenize(string)
+    result = evaluate token_stacks
     result
   end
 
-  def self.evaluate(nums, operations)
-    result = nums.pop.value
-    until operations.empty?
-      result = operations.pop.call(result, nums.pop.value)
+  def self.evaluate(tokens)
+    token_stacks[:numeric] << tokens.pop.value
+    until tokens.empty?
+      operation = tokens.pop
+      if [:*, :/].include? operation.value
+        left = tokens.pop.value
+        right = token_stacks[:numeric].pop
+        token_stacks[:numeric] << operation.call(left, right)
+      elsif [:+, :-].include? operation.value
+        token_stacks[:operation] << operation
+        token_stacks[:numeric] << tokens.pop.value
+      end
     end
-    result
+    until token_stacks[:operation].empty?
+      left = token_stacks[:numeric].pop
+      right = token_stacks[:numeric].pop
+      operation = token_stacks[:operation].pop
+      token_stacks[:numeric] << operation.call(left,right)
+    end
+    token_stacks[:numeric].pop
   end
 
   def self.tokenize(string)
+    @tokens = []
     input = string.dup
     until input.empty?
       scanners.each { |scanner|
         token = scanner[:pattern].match(input)
         if token
-          token_stacks[scanner[:category]] << Calc::Token.new(scanner[:category], token[0])
+          @tokens << Calc::Token.new(scanner[:category], token[0])
           input = input[token[0].length..-1]
         end
       }
     end
-    token_stacks[:numeric].reverse!
-    token_stacks[:operation].reverse!
-    return token_stacks[:numeric], token_stacks[:operation]
+    return @tokens
   end
 end
